2e0850cf44f682788ee2e59ea0023d2a33a895a2
todaka0202 <yizaihugao@gmail.com>
2024-06-20 12:20:33 +0900

    work4終わりました

--- work4.py
+++ work4.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random
+
 # ↓↓↓ お約束のコード ↓↓↓
 window = tk.Tk()
 window.title("GUI App")
@@ -7,22 +9,37 @@ bg_color = "#333333"  # ダークグレー
 fg_color = "#FFFFFF"  # 白
 window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
+
+label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
+
 meibo = []
-def button_action():  # 関数の定義 ※ボタンが押されたときの動き
+def button_action1():  # 関数の定義 ※ボタンが押されたときの動き
     meibo.append(entry1.get())
     name_list = ""
     for a in meibo:
         name_list += f"{a}\n"  # 入力値を取得
-    label1.config(text=f"{name_list}\n ")  # 画面に出力
+    label1.config(text=f"{name_list}\n ")
+    
+def button_action2():
+    a = random.choice(meibo)
+    label2.config(text=a)
+    
+      # 画面に出力
 # 入力フィールドの作成
 entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
 entry1.pack(pady=10)
 # ボタンの作成
-button1 = tk.Button(window, text="追加", command=button_action)
+button1 = tk.Button(window, text="追加", command=button_action1)
 button1.pack(pady=10)
+
+button2 = tk.Button(window, text="ランダム選択", command=button_action2)
+button2.pack(pady=10)
 # 出力ラベルの作成
-label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
-label1.pack(pady=10)
+
+
+label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label2.pack(pady=10)
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
 # ↑↑↑ お約束のコード ↑↑↑
