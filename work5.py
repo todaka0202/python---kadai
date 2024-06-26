26dc1866a19b7f6383ed96c584ba7fc246eebefc
todaka0202 <yizaihugao@gmail.com>
2024-06-26 10:39:06 +0900

    終わりました

--- work5.py
+++ work5.py
@@ -17,17 +17,15 @@ window.configure(bg=bg_color)
 # ↑↑↑ お約束のコード ↑↑↑
 str_list=["python","tkinter","javascript","toukyou"]
 
-question = random.choice(str_list)
 
 def button_action():  # 関数の定義 ※ボタンが押されたときの動き
     user_input = entry1.get()  # 入力値を取得
     
-    if user_input == question:
-        question = random.choice(str_list)
-    else:
-        label2.config(text=f"違います！")  # 画面に出力
+    if label1.cget("text") == user_input:
+        label1.config(text=random.choice(str_list))
+         # 画面に出力
 
-label1 = tk.Label(window, text=question, bg=bg_color, fg=fg_color)
+label1 = tk.Label(window, text=random.choices(str_list), bg=bg_color, fg=fg_color)
 label1.pack(pady=10)
 
 # 入力フィールドの作成
@@ -39,8 +37,8 @@ button1 = tk.Button(window, text="OK", command=button_action)
 button1.pack(pady=10)
 
 # 出力ラベルの作成
-label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
-label2.pack(pady=10)
+# label2 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+# label2.pack(pady=10)
 
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
